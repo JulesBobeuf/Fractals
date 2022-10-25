fce8b0d984c3d34b6f40a64529020aa116955ca4
JAKOBOWSKI Aymeric <aymeric_jakobowski@ens.univ-artois.fr>
2022-10-25 15:16:34 +0200

    Ajout premier masque

---
+++ src/main/java/fr/univartois/butinfo/fractals/image/MasqueDecorateur.java
@@ -0,0 +1,49 @@
+/**
+ * Ce fichier fait partie du projet sae-2022-2023.
+ *
+ * (c) 2022 aymeric.jakobowski
+ * Tous droits réservés.
+ */
+
+package fr.univartois.butinfo.fractals.image;
+
+import java.awt.Color;
+
+/**
+ * Le type MasqueDecorateur
+ *
+ * @author aymeric.jakobowski
+ *
+ * @version 0.1.0
+ */
+public class MasqueDecorateur implements IPalettesCouleurs {
+
+    /**
+     * L'attribut paletteCouleurMasque...
+     */
+    private IPalettesCouleurs paletteCouleurMasque;
+
+    /**
+     * Crée une nouvelle instance de MasqueDecorateur.
+     * 
+     * @param palettesCouleurs
+     */
+    public MasqueDecorateur(IPalettesCouleurs paletteCouleurMasque) {
+        this.paletteCouleurMasque = paletteCouleurMasque;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see fr.univartois.butinfo.fractals.image.IPalettesCouleurs#getColor(int)
+     */
+    @Override
+    public Color getColor(int nbIterations, int nbMaxIterations) {
+        Color c1 = paletteCouleurMasque.getColor(nbIterations, nbMaxIterations);
+        float rouge = c1.getRed();
+        float vert =c1.getGreen();
+        float bleu = c1.getBlue();
+        return new Color(0, vert, bleu, 1);
+    }
+
+}
