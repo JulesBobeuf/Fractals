fb3a332bd9b16ee83a20f76ae21c7d483d43a706
Thomas Santoro <thomas_santoro@ens.univ-artois.fr>
2022-10-25 17:07:45 +0200

    Question 16 Fix #12

--- src/main/java/fr/univartois/butinfo/fractals/complex/Point.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/Point.java
@@ -33,9 +33,21 @@ public class Point {
         return complex.getImaginaryPart();
     }
     
+    /**
+     * @param complexe1
+     * @param complexe2
+     * @return
+     */
     public double DistanceEntre2Points(Complex complexe1,Complex complexe2) {
         Point point1 = new Point(complexe1);
         Point point2 = new Point(complexe2);
         return Math.sqrt((Math.pow(point1.getX()-point2.getX(),2))+Math.pow(point1.getY()-point2.getY(),2));
+    }
+    
+    /**
+     * @return
+     */
+    public Complex PointEnComplex() {
+        return complex;
     }
 }
