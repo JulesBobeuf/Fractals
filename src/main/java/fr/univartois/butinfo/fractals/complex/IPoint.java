53946871d0c8537b90a50f61da94858548f3d387
Thomas Santoro <thomas_santoro@ens.univ-artois.fr>
2022-10-26 13:40:20 +0200

    ajout interface de Point

---
+++ src/main/java/fr/univartois/butinfo/fractals/complex/IPoint.java
@@ -0,0 +1,14 @@
+package fr.univartois.butinfo.fractals.complex;
+
+
+public interface IPoint {
+    /**
+     * @return
+     */
+    double DistanceEntre2Points();
+    
+    /**
+     * @return
+     */
+    Complex PointEnComplex();
+}
