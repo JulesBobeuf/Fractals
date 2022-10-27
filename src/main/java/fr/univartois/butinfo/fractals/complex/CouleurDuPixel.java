2a93a551a11cdd7c31ff10da0b1b1fe0d03f4e74
Thomas Santoro <thomas_santoro@ens.univ-artois.fr>
2022-10-27 13:00:00 +0200

    fin

--- src/main/java/fr/univartois/butinfo/fractals/complex/CouleurDuPixel.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/CouleurDuPixel.java
@@ -7,6 +7,10 @@
 
 package fr.univartois.butinfo.fractals.complex;
 
+import java.awt.image.BufferedImage;
+import fr.univartois.butinfo.fractals.image.AdaptateurImage;
+import fr.univartois.butinfo.fractals.image.IFractalImage;
+import fr.univartois.butinfo.fractals.image.Pixel;
 
 /**
  * Le type CouleurDuPixel
@@ -16,6 +20,46 @@ package fr.univartois.butinfo.fractals.complex;
  * @version 0.1.0
  */
 public class CouleurDuPixel {
-    
-}
 
+    /**
+     * L'attribut suite...
+     */
+    private ISuiteChaotique suite;
+
+    /**
+     * L'attribut plan...
+     */
+    private IPlanComplexe plan;
+    
+    /**
+     * L'attribut n...
+     */
+    private int n;
+
+    /**
+     * Crée une nouvelle instance de CouleurDuPixel.
+     * @param suite
+     * @param plan
+     * @param n
+     */
+    public CouleurDuPixel(ISuiteChaotique suite, IPlanComplexe plan, int n) {
+        this.suite = suite;
+        this.plan = plan;
+        this.n = n;
+    }
+
+    /**
+     * 
+     */
+    public void getCouleur() {
+        for (int wi = 0; wi < plan.getWidth(); wi++) {
+            for (int he = 0; he < plan.getHeight(); he++) {
+                IterateurDeSuiteChaotique iterator = new IterateurDeSuiteChaotique(suite, n);
+                while (iterator.hasNext()) {
+                    iterator.next();
+                }
+                //pixel.setColor(palette.getColor(count, max));
+            }
+        }
+    }
+}
