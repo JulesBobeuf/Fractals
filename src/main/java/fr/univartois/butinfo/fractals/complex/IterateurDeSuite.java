c6d9fbb886b07bf5c5c14f9d58bd213977481cc4
Jules B <bobeuf.jules@gmail.com>
2022-10-27 10:01:58 +0200

    debug git conflits version blabla

--- src/main/java/fr/univartois/butinfo/fractals/complex/IterateurDeSuite.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/IterateurDeSuite.java
@@ -7,41 +7,25 @@ import java.util.Iterator;
  * @author thomas
  *
  */
-public class IterateurDeSuite implements Iterator<IComplex> {
-
-    /**
-     * L'attribut suite...
-     */
+public class IterateurDeSuite  implements Iterator<IComplex>{
+    
     private ISuitesComplexesRecurrentes suite;
-
-    /**
-     * L'attribut valeur...
-     */
     private IComplex valeur;
 
-    /**
-     * L'attribut nbIteration...
-     */
     private int nbIteration;
-
-    /**
-     * L'attribut n...
-     */
-    private int n;
-
-    /**
-     * @param s
-     * @param n
-     */
+    
+    private int n; // donner une limite
+    
     public IterateurDeSuite(ISuitesComplexesRecurrentes s, int n) {
-        this.suite = s;
-        this.n = n;
-        nbIteration = 0;
+        this.suite=s;
+        this.n=n;
+        this.valeur=s.getJ0();
+        nbIteration=0;
     }
 
     @Override
     public boolean hasNext() {
-        if (nbIteration <= n) {
+        if ((nbIteration<=n) && (valeur.abs()<2)) {
             return true;
         }
         return false;
@@ -49,9 +33,20 @@ public class IterateurDeSuite implements Iterator<IComplex> {
 
     @Override
     public IComplex next() {
-        valeur = suite.valeurProchainTerme(valeur);
-        nbIteration++;
+        if (nbIteration==0) {
+            nbIteration++;
+            return valeur;
+        }
+        if (this.hasNext()) {
+            valeur = suite.valeurProchainTerme(valeur);
+            nbIteration++;
+            return valeur;
+        }
+        else {
+            return valeur;
+        }
+    }
+    public IComplex getValeur() {
         return valeur;
     }
-
 }
